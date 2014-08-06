da5f7a3bdcfbb3fe4fab51985a3d1e19b328af27
clairtonluz <clairton.c.l@gmail.com>
2014-08-06 10:08:27 -0300

    adicionando bootstrap

--- src/main/java/io/github/clairton/controller/ExampleController.java
+++ src/main/java/io/github/clairton/controller/ExampleController.java
@@ -6,5 +6,30 @@ import javax.inject.Named;
 @Named
 @ManagedBean
 public class ExampleController {
+  
+  private String username;
+  private String password;
+  
+  public void login(){
+    System.out.println("User = " + username);
+    System.out.println("Password = " + password);
+  }
 
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  
 }
